d3055fdc38d506a981cd18e3bebfd29f4dfa22d2
LuisC73 <luiscastroc11@gmail.com>
2022-10-27 12:02:04 -0500

    se agrega componente de alt

---
+++ Texto alternativo/main.js
@@ -0,0 +1,17 @@
+setTimeout(() => {
+    document.querySelector('#imgTable img').setAttribute('alt','tramiApp')
+    document.querySelector('#imgTable a').setAttribute('aria-label','tramiApp')
+  }, 4000)
+
+
+  let imagesNoticias = document.querySelectorAll('.w3_agile_event_grid1 img')
+  imagesNoticias.forEach(img => img.setAttribute('alt','imagenNoticias'))
+
+  let enlaces = document.querySelectorAll('.news-desc a')
+  enlaces.forEach(enlace => enlace.setAttribute('aria-label','enlace sala de prensa'))
+
+  let imagesPop = document.querySelectorAll('.modal-body img')
+  imagesPop.forEach(img => img.setAttribute('alt','imagen pop up'))
+
+  let enlacesPop = document.querySelectorAll('.modal-body a')
+  enlacesPop.forEach(enlace => enlace.setAttribute('aria-label','enlace pop up'))
