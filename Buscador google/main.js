ae2fd8e6be475be665529c29238b73e2c16059ac
LuisC73 <luiscastroc11@gmail.com>
2022-10-31 11:27:00 -0500

    se agrega placeholder a buscador

---
+++ Buscador google/main.js
@@ -0,0 +1,10 @@
+// Inicio Placeholder en buscador
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document
+      .getElementById("gsc-i-id1")
+      .setAttribute("placeholder", "¿Qué deseas buscar hoy?");
+  }, 4000);
+});
+
+// Fin Placeholder en buscador
