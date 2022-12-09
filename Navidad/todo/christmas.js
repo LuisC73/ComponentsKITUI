e261af18a838e0551dcef35151a3c03c5b0adf4b
LuisC73 <luiscastroc11@gmail.com>
2022-12-09 15:11:13 -0500

    se soluciona bug de nieve

--- Navidad/todo/christmas.js
+++ Navidad/todo/christmas.js
@@ -1,11 +1,13 @@
-var t = setInterval(function () {
-  var documentHeight = $(document).height();
-  var startPositionLeft = Math.random() * $(document).width() - 100;
-  var startOpacity = 0.5 + Math.random();
-  var sizeFlake = 10 + Math.random() * 46;
-  var endPositionTop = documentHeight - 40;
-  var endPositionLeft = startPositionLeft - 100 + Math.random() * 200;
-  var durationFall = documentHeight * 10 + Math.random() * 5000;
+let t = setInterval(function () {
+  let documentHeight = $(document).height();
+  let mediaQuery = window.matchMedia("(max-width: 800px)");
+  let startPositionLeft = Math.random() * $(document).width() - 100;
+  if (mediaQuery.matches) {startPositionLeft = Math.random() * $(document).width() - 130}
+  let startOpacity = 0.5 + Math.random();
+  let sizeFlake = 10 + Math.random() * 46;
+  let endPositionTop = documentHeight - 40;
+  let endPositionLeft = startPositionLeft - 100 + Math.random() * 200;
+  let durationFall = documentHeight * 10 + Math.random() * 5000;
   $("#flake")
     .clone()
     .appendTo("body")
@@ -28,11 +30,11 @@ var t = setInterval(function () {
     );
 }, 500);
 
-var snow = {};
-var snowflex = {};
+let snow = {};
+let snowflex = {};
 
 snowflex.create = function () {
-  var flex = document.createElement("div");
+  let flex = document.createElement("div");
   flex.innerHTML = "&#10052;";
   flex.style.fontSize = 10 + Math.random() * 20 + "px";
   flex.style.top = -50 + Math.random() * 20 + "px";
@@ -45,9 +47,9 @@ snowflex.create = function () {
 };
 
 snow.snowflex = function () {
-  var flex = snowflex.create();
-  var x = -1 + Math.random() * 2;
-  var t = setInterval(function () {
+  let flex = snowflex.create();
+  let x = -1 + Math.random() * 2;
+  let t = setInterval(function () {
     flex.style.top = parseInt(flex.style.top) + 5 + "px";
     flex.style.left = parseInt(flex.style.left) + x + "px";
     if (parseInt(flex.style.top) > 1500) {
@@ -58,14 +60,14 @@ snow.snowflex = function () {
 };
 
 snow.storm = function () {
-  var t = setInterval(function () {
+  let t = setInterval(function () {
     snow.snowflex();
   }, 500);
 };
 
 //snow.storm();
 
-var fog = {};
+let fog = {};
 
 fog.draw = function (ctx, x, y) {
   ctx.fillStyle = "rgba( 255, 255, 255, " + Math.random() + " )";
@@ -75,10 +77,10 @@ fog.draw = function (ctx, x, y) {
 };
 
 fog.start = function () {
-  var ctx = document.getElementById("canvas").getContext("2d");
-  var x = 0;
-  var y = 0;
-  var t = setInterval(function () {
+  let ctx = document.getElementById("canvas").getContext("2d");
+  let x = 0;
+  let y = 0;
+  let t = setInterval(function () {
     x = 300 + 300 * Math.sin(x);
     y = 300 + 300 * -Math.cos(x);
 
