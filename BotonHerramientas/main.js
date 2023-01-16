8582eb9f5a392e482c7db46212a2032b5523bdf0
LuisC73 <luiscastroc11@gmail.com>
2023-01-16 11:22:48 -0500

    Se realiza documentacion de boton de herramientas

--- BotonHerramientas/main.js
+++ BotonHerramientas/main.js
@@ -1,26 +1,34 @@
 import buttonInformation from "./dataInformation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Se inicializa constante con valor del boton
+  // Se inicializa constante con valor del boton y contenedor de opciones
   const buttonFloat = document.querySelector(".floatButton"),
     buttonsContainer = document.querySelector(".floatButton__options");
 
+  // Se crea fragmento para insertar elementos.
   const fragmentContent = document.createDocumentFragment();
 
+  // Apartir de importar la informacion de cada opcion se recorre el objeto
   for (const i in buttonInformation) {
+    // Se crea elemento tipo enlace (a)
     let button = document.createElement("a");
-
+    // Se agrega clase principal para elemento tipo opcion
     button.classList.add("floatButton__item");
-    button.classList.add(
-      `floatButton__item--color${buttonInformation[i].color}`
-    );
+    //Se cambia color del elemento
+    button.style.background = `${buttonInformation[i].color}`;
+    button.style.boxShadow = `0 20px 20px -10px ${buttonInformation[i].color}`;
 
+    // Se ingresa atributo href al elemento
+    button.setAttribute("href", buttonInformation[i].link);
+
+    // Si el elemento es tipo icono se agrega el siguiente contenido
     if (buttonInformation[i].link !== "none") {
       button.innerHTML = `<i class="${buttonInformation[i].icon} floatButton__icon"></i>
       <span class="floatButton__tooltip"
         >${buttonInformation[i].text}</span>`;
     }
 
+    // Si el elemento es tipo imagen se agrega el siguiente contenido
     if (buttonInformation[i].img !== "none") {
       button.innerHTML = `<img
       src="${buttonInformation[i].img}"
@@ -33,13 +41,14 @@ document.addEventListener("DOMContentLoaded", () => {
       >${buttonInformation[i].text}</span>`;
     }
 
-    button.setAttribute("href", buttonInformation[i].link);
-
+    // Se insertan todos los elementos en el fragmento
     fragmentContent.appendChild(button);
   }
 
+  // Se inserta fragmento en contenedor principal de opciones
   buttonsContainer.appendChild(fragmentContent);
 
+  // Se crea funcion para activacion del boton y animacion a sus opciones
   function activeButtons() {
     buttonFloat.classList.toggle("floatButton--active");
 
