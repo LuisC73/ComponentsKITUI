fae66c649d76039893908afc661ad7f4a90c14f5
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2023-02-12 17:33:30 -0500

    se agrega paginador

---
+++ BuscadorNormatividad(Beta)/prueba/data.js
@@ -0,0 +1,46 @@
+const dataInfo = [
+  {
+    title: "Articulo",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+  {
+    title: "Articulo 2",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+  {
+    title: "Articulo 3",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+  {
+    title: "Articulo 4",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+  {
+    title: "Articulo 5",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+  {
+    title: "Articulo 6",
+    clasf: "Prueba",
+    fecha: "17 de julio",
+    año: "2024",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+  },
+];
+
+export default dataInfo;
