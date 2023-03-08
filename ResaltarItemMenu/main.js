dc5ff896503c6b2ebf1a739d14512bfae6d5628e
LuisC73 <luiscastroc11@gmail.com>
2023-03-08 15:16:53 -0500

    Se agrega documentación

--- ResaltarItemMenu/main.js
+++ ResaltarItemMenu/main.js
@@ -1,6 +1,9 @@
+/* Se agrega un listener al evento "DOMContentLoaded" que se dispara una vez que la página ha cargado completamente. */
 document.addEventListener("DOMContentLoaded", () => {
+  /* Se obtiene la URL de la página actual en minúsculas y se almacena en la variable windowUrl. */
   let windowUrl = window.location.href.toLowerCase();
 
+  /** Se define un array llamado allModules que contiene los nombres de módulos. */
   const allModules = [
     "transparencia",
     "ciudadanos",
@@ -11,13 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     "notificaciones",
   ];
 
+  /** Se obtiene el primer elemento que coincide con el selector 'Selector[data-item="inicio"]' y se almacena en la variable initItem. */
   const initItem = document.querySelector('Selector[data-item="inicio"]');
+  /** Se obtienen todos los elementos que coinciden con el selector especificado y se almacenan en la variable allMenuModule como un array. */
   const allMenuModule = [...document.querySelectorAll("Ingresar selector")];
 
+  /** Se define una función llamada toggleMenuActiveClass que toma un argumento item y agrega o quita la clase menu__active al elemento. */
   const toggleMenuActiveClass = (item) => {
     item.classList.toggle("menu__active");
   };
 
+  /** Se define una función llamada checkMenuItem que toma dos argumentos, modulePage y itemsMenu. 
+   * La función recorre todos los elementos del array itemsMenu, obtiene el valor del atributo data-item
+   *  y lo convierte en minúsculas. Si el valor incluye el nombre del módulo pasado como argumento, la función llama a 
+   * toggleMenuActiveClass para agregar o quitar la clase menu__active. */
   const checkMenuItem = (modulePage, itemsMenu) => {
     itemsMenu.forEach((item) => {
       const textItem = item.dataset.item.toLowerCase();
@@ -27,6 +37,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
+  /** Se define una función llamada checkMenuItems que recorre el array allModules. 
+   * Para cada elemento del array, la función convierte el nombre del módulo en minúsculas y verifica si la URL 
+   * de la página actual contiene el nombre del módulo. Si es así, la función llama a checkMenuItem para agregar o quitar 
+   * la clase menu__active a los elementos del menú correspondientes. */
   const checkMenuItems = () => {
     allModules.forEach((element) => {
       const modulePage = element.toLowerCase();
@@ -36,5 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
+  /** Se llama a la función checkMenuItems una vez que la página ha cargado completamente. */
   checkMenuItems();
 });
