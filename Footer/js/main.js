25c8e7c599460a15b7b470bbc78638a945b4fd3a
LuisC73 <luiscastroc11@gmail.com>
2023-04-10 17:20:36 -0500

    Se mejora diseño del footer con sass

---
+++ Footer/js/main.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const allDropMenu = document.querySelectorAll(".footerWeb__ul"),
+//     allToggleMenu = document.querySelectorAll(".footerWeb__option");
+
+//   allToggleMenu.forEach((option, indexOption) => {
+//     allDropMenu.forEach((menu, indexMenu) => {
+//       document.addEventListener("click", (e) => {
+//         if (e.target === option) {
+//           if (indexMenu === indexOption) {
+//             menu.classList.toggle("footerWeb__ul--active");
+//           }
+//         }
+//       });
+//     });
+//   });
+// });
