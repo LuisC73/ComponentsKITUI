b36f65d7f2021e613e0469a483432803da3cc757
LuisC73 <luiscastroc11@gmail.com>
2023-04-10 14:13:38 -0500

    Se mejora header

---
+++ Header/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    const inputGoogle = document.getElementById("gsc-i-id1");
+    inputGoogle.setAttribute("placeholder", "Buscar en la entidad");
+  }, 2000);
+});
